4b49c2a01cca97fe410d2936276563eb8b233bbb
Vera Var <primavera1919@yandex.ru>
2020-08-20 01:05:57 +0300

    Testng and tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
+    public void shouldReturn999IfAmountIs1() {
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn1IfAmountIs999() {
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual,expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturnZeroIfAmountIs1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+}
+
